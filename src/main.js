e340831830fe9f0e2b4cdf2d5db708625515aa32
Nicolas Payot <nicolas.payot@gmail.com>
2018-11-18 19:07:07 +0100

    Add corrections

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
-console.log('Hello, World!');
+import { Store } from './store';
+import { View } from './View';
+
+const store = new Store();
+const view = new View();
+
+const render = () => {
+  const { total, completed } = store.count();
+  const allCompleted = total === completed;
+
+  view.render(store.todoList, allCompleted, $todoList => {
+    $todoList.forEach($todo => {
+
+      view.onToggleTodo($todo, checked => {
+        store.toggleTodo($todo.getAttribute('id'), checked);
+        render();
+      });
+
+      view.onRemoveTodo($todo, () => {
+        store.removeTodo($todo.getAttribute('id'));
+        render();
+      });
+
+      view.onEditTodo($todo, () => {
+        const $input = view.editTodo($todo);
+        view.onUpdateTodo($input, newTitle => {
+          store.updateTodo($todo.getAttribute('id'), newTitle);
+          render();
+        });
+      });
+
+    });
+  });
+};
+
+render();
+
+view.onAddTodo(title => {
+  store.addTodo(title);
+  render();
+});
+
+view.onToggleAllTodos(checked => {
+  store.toggleAllTodos(checked);
+  render();
+});
